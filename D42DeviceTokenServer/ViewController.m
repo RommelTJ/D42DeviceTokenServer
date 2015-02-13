21c556b145c991190a4a1cad04329f51f2a8f72f
Rommel Rico <rommeltj@gmail.com>
2015-02-12 20:32:44 -0800

    Initial View Controller

--- D42DeviceTokenServer/ViewController.m
+++ D42DeviceTokenServer/ViewController.m
@@ -8,13 +8,49 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+#define DEVICE_SERVICE_TYPE @"_http._tcp"
+#define DEVICE_SERVICE_NAME @"com.rommelrico.DeviceToken"
+#define DEVICE_SERVICE_PORT 50123
 
+@interface ViewController () <NSNetServiceDelegate>
 
+@property (weak) IBOutlet NSTextField *myServerStatusLabel;
+@property NSNetService *myService;
 
 @end
 
 @implementation ViewController
+
+- (IBAction)doStartHTTPServerButton:(NSButton *)button {
+    //NSButton *button = sender;
+    static BOOL advertising = YES;
+    if (advertising) {
+        //Start Advertising
+        self.myService = [[NSNetService alloc] initWithDomain:@"" type:DEVICE_SERVICE_TYPE name:DEVICE_SERVICE_NAME port:DEVICE_SERVICE_PORT];
+        self.myService.delegate = self;
+        [self.myService publish];
+        //Change button text
+        [button setTitle:@"Stop HTTP Server"];
+    } else {
+        //Stop Advertising
+        [self.myService stop];
+        [button setTitle:@"Start HTTP Server"];
+    }
+    advertising = !advertising;
+    
+}
+
+-(void)netServiceDidPublish:(NSNetService *)sender {
+    self.myServerStatusLabel.stringValue = @"Server did publish!";
+}
+
+-(void)netService:(NSNetService *)sender didNotPublish:(NSDictionary *)errorDict {
+    self.myServerStatusLabel.stringValue = @"Server did not publish!";
+}
+
+-(void)netServiceDidStop:(NSNetService *)sender {
+    self.myServerStatusLabel.stringValue = @"Server did stop!";
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
